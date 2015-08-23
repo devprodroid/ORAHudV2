b9610f9ffc023e117b19b8c201e8e4744ff10145
robert <robertbremer83@gmail.com>
2015-08-23 16:22:44 +0200

    Move arrows next to the altitudemarker

--- app/src/main/java/devprodroid/orahudclient/glSurface/UpArrow.java
+++ app/src/main/java/devprodroid/orahudclient/glSurface/UpArrow.java
@@ -26,10 +26,10 @@ public class UpArrow {
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
     static float squareCoords[] = {
-            -0.8f, 0.4f, 0.0f,   // top left
-            -0.8f, 0.01f, 0.0f,   // bottom left
-            -0.7f, 0.01f, 0.0f,   // bottom right
-            -0.7f, 0.4f, 0.0f}; // top right
+            0.8f, 0.4f, 0.0f,   // top left
+            0.8f, 0.01f, 0.0f,   // bottom left
+            0.7f, 0.01f, 0.0f,   // bottom right
+            0.7f, 0.4f, 0.0f}; // top right
 
     private short drawOrder[] = {0, 1, 2, 0, 2, 3}; // order to draw vertices
     private int mPositionHandle;
